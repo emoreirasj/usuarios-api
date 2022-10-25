64ad855cfdb570186c047495546b28d522c1569d
emoreirasj <emsj89@gmail.com>
2022-10-24 21:15:56 -0300

    Primeiro commit

---
+++ usuarios.js
@@ -0,0 +1,20 @@
+//Grupo 8
+
+//Edson - emoreirasj - Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros
+
+
+
+//Sergio - SergioRibeiro8 - Aluno 2 - Responsável por criar a função de listar todos os usuários acima de 18 anos.
+
+
+
+//Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
+
+
+
+//Luiz - luizngustavo -  Aluno 4 - Responsável por criar a função de adicionar um usuário
+
+
+
+
+//Aluno 5- Responsável por criar a função de remover um usuário
