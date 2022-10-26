cfc96028986fa11e323d0223b6252fa9a6ce003e
luizngustavo <116230060+luizngustavo@users.noreply.github.com>
2022-10-26 13:14:40 -0300

    teste

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,5 @@
 //Grupo 8
-let usuários = require('./database/users.json');
+let usuarios = require('./database/users.json');
 
 //Edson - emoreirasj - Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros
 
@@ -15,8 +15,28 @@ let usuários = require('./database/users.json');
 
 //Luiz - luizngustavo -  Aluno 4 - Responsável por criar a função de adicionar um usuário
 
+const adicionarUsuario = (usuario) => {
+    
+    const usuarioParaAdicionar ={
+        ...usuario,
+        ativo: true
+    }
 
+    usuarios.push(usuarioParaAdicionar)
+    
+    return usuarioParaAdicionar
+}
+let usuariosParaAdicionar ={
+    "id": 7,
+    "nome": "luiz gustavo",
+    "idade": 22,
+    "sexo": "M",
+    "email": "luizgustavo@gmail.com",
+    "telefone": "999999999"
+  }
 
+adicionarUsuario(usuariosParaAdicionar)
+console.log(usuarios)
 
 //Aluno 5- Responsável por criar a função de remover um usuário
 
