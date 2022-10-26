1aa32100a69d85b3f73920cd272ed91bb7980a84
emoreirasj <emsj89@gmail.com>
2022-10-26 20:40:13 -0300

    Correção dos exercicíos.

--- usuarios.js
+++ usuarios.js
@@ -4,14 +4,28 @@ let usuarios = require('./database/users.json');
 //Edson - emoreirasj - Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros
 //Responsável por criar a função de listar todos os usuários acima de 18 anos.
 
-const maiores = () => console.log(usuarios.filter(idade => idade.idade > 18))
+console.log('Usuários acima de 18')
+console.log()
 
-maiores()
+const maiores = () => console.log(usuarios.filter((usuario) => usuario.idade > 18))
+
+maiores()   
+
 //Rubio - Rubio01 - Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
-AdicionarPropriedade =(usuarios)=> console.log(usuarios.map((usuario)=>({...usuario, ativo:'true'})))
-AdicionarPropriedade(usuarios)
+
+
+console.log('Lista com ativo')
+
+const listaAtivo = () => { 
+    usuarios.map((usuario) => ({...usuario, ativo: 'true'}))
+return listaAtivo
+}
+
+console.log(listaAtivo)
 
 //Luiz - luizngustavo -  Aluno 4 - Responsável por criar a função de adicionar um usuário
+
+console.log('Usuário adicionado')
 
 const adicionarUsuario = (usuario) => {
     
@@ -31,9 +45,24 @@ let usuariosParaAdicionar ={
     "sexo": "M",
     "email": "luizgustavo@gmail.com",
     "telefone": "999999999"
-  }
+}
 
 adicionarUsuario(usuariosParaAdicionar)
 console.log(usuarios)
 
 //Aluno 5- Responsável por criar a função de remover um usuário
+
+const removerUsuario = (idUsuario) => { 
+    const usuario = usuarios.findIndex((usuario) => usuario.id ==idUsuario)
+
+    if(usuario.index < 0){
+        console.log("Usuário não encontrado")
+        return;
+    } 
+
+    usuarios.splice(usuarioIndex, 1)
+
+}
+
+removerUsuario(1)
+console.log("Usuário removido: ", usuarios);
