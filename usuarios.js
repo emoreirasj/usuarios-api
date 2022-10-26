2ce276367e35b437ced6703e3da279a68f303913
Edson Moreira dos Santos Junior <emsj89@gmail.com>
2022-10-26 12:30:00 -0300

    Função de listar maiores de 18 (#2)

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,5 @@
 //Grupo 8
-let usuários = require('./database/users.json');
+let usuarios = require('./database/users.json');
 
 //Edson - emoreirasj - Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros
 
@@ -7,8 +7,9 @@ let usuários = require('./database/users.json');
 
 //Sergio - SergioRibeiro8 - Aluno 2 - Responsável por criar a função de listar todos os usuários acima de 18 anos.
 
+const maiores = () => console.log(usuarios.filter(idade => idade.idade > 18))
 
-
+maiores()
 //Rubio - Rubio01 - Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
 AdicionarPropriedade =(usuarios)=> console.log(usuarios.map((usuario)=>({...usuario, ativo:'true'})))
 AdicionarPropriedade(usuários)
@@ -19,5 +20,3 @@ AdicionarPropriedade(usuários)
 
 
 //Aluno 5- Responsável por criar a função de remover um usuário
-
-const removerUsuario = () =>{}
